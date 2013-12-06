f7d9c19b332849595a895505788087964ccfdd50
itspeedster <itspeedster@gmail.com>
2013-12-06 11:42:33 +0700

    Create HelloEclipseGit class

---
+++ src/com/itspeedster/eclipsegit/HelloEclipseGit.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.itspeedster.eclipsegit;
+
+/**
+ * @author itspeedster
+ * 
+ */
+public class HelloEclipseGit {
+
+	public static void main(String args[]){
+		System.out.println("Hello Eclipse Git");
+	}
+}
